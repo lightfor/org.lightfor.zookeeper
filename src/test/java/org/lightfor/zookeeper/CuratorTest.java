c037e322db6943ea0317978af11b6c669f9269f7
lightfor <death_light@foxmail.com>
2017-08-02 23:29:05 +0800

    lock sample

---
+++ src/test/java/org/lightfor/zookeeper/CuratorTest.java
@@ -0,0 +1,41 @@
+package org.lightfor.zookeeper;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * curator test
+ * Created by Light on 2017/7/29.
+ */
+public class CuratorTest {
+
+    public void test() throws Exception {
+
+        String zookeeperConnectionString = "192.168.1.230:2181";
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+        client.start();
+
+        //client.create().forPath("/my/path", myData)
+
+        InterProcessMutex lock = new InterProcessMutex(client, "lockPath");
+        if ( lock.acquire(2, TimeUnit.SECONDS) )
+        {
+            try
+            {
+                // do some work inside of the critical section here
+            }
+            finally
+            {
+                lock.release();
+            }
+        }
+
+
+    }
+}
